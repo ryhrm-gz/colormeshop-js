7d71ffe8a148dd87348966d52c8a8994e203b44a
ryhrm-gz <gyozaoisii@icloud.com>
2022-01-07 14:54:09 +0900

    update jsdoc

--- src/createClient.ts
+++ src/createClient.ts
@@ -59,6 +59,12 @@ export const createClient = ({ accessToken }: ColormeClient) => {
     getStat: (params?: ColormeGetStatParams) => getStat(client, params),
     /** 受注データの取得 */
     getSale: (params: ColormeGetSaleParams) => getSale(client, params),
+    /**
+     * 受注データの更新
+     * 該当受注の決済がAmazon Pay、LINE Pay、または楽天ペイ（オンライン決済）である場合は、熨斗・メッセージカード・ラッピングの手数料を更新すると、決済金額が自動的に購入者に請求もしくは返金されます。
+     *
+     * ただし、LINE Payの場合は、決済金額の変更は減額のみ行うことができます。
+     */
     updateSale: (saleId: number, params: { sale: ColormeUpdateSaleParams }) =>
       updateSale(client, saleId, params),
   };
