51748fed514ff4c46bb576f2512702eaf8b081ac
ryhrm-gz <gyozaoisii@icloud.com>
2024-02-25 03:19:57 +0900

    Remove unnecessary function calls in getDeliveries and getDeliveryDateSetting

--- src/apis/deliveries/getDeliveryDateSetting.ts
+++ src/apis/deliveries/getDeliveryDateSetting.ts
@@ -8,7 +8,3 @@ export const getDeliveryDateSetting = async (apiKey: string) => {
     'GET'
   );
 };
-
-getDeliveryDateSetting(
-  '5920abd3832915c2561ee9eb073d7c993a57f82887ed9f5ff777b1fd3f593480'
-).then(console.log);
